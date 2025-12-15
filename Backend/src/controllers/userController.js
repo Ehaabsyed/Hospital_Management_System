7de792b2e838af3c8cf32007dd9a3579a7f9580b
Ehaabsyed <syedehaab12@gmail.com>
2025-12-15 23:32:05 +0530

    Changes in user ctr

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -82,7 +82,7 @@ export const getallPatients = async (req, res) => {
 }
 //login function
 export const userlogin = async (req, res) => {
-    // console.log(req.body);
+    // console.log(req.body);  
 
     try {
         const { email, password } = req.body.data;
