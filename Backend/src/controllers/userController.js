19318161538bca4c02ffce3b6ac959fb4a276d71
Ehaabsyed <syedehaab12@gmail.com>
2025-12-16 00:33:09 +0530

    changes in Users login page

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -101,7 +101,7 @@ export const userlogin = async (req, res) => {
   console.log(req.body);
 
   try {
-    const { email, password } = req.body.data;
+    const { email, password } = req.body;
     console.log(email,password);
 
     let existUser = await userModel.findOne({ email }).select("+password");
